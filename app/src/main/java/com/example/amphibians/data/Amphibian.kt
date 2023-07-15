e108cca0c146392e6f25361eae6a7fa3034a048d
Mirror83 <glenochieng045@gmail.com>
2023-07-15 08:24:57 +0300

    Added Amphibian data class

--- app/src/main/java/com/example/amphibians/data/Amphibian.kt
+++ app/src/main/java/com/example/amphibians/data/Amphibian.kt
@@ -1,7 +1,13 @@
 package com.example.amphibians.data
 
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class Amphibian(
     val name: String,
+    val type: String,
     val description: String,
+    @SerialName("img_src")
     val imgSrc: String
 )
