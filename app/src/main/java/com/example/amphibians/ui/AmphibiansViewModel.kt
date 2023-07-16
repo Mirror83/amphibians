aa1ef51876680b44124ff5177697dcadeaab4509
Mirror83 <glenochieng045@gmail.com>
2023-07-16 08:05:55 +0300

    Injected AmphibiansRepository into AmphibiansViewModel

--- app/src/main/java/com/example/amphibians/ui/AmphibiansViewModel.kt
+++ app/src/main/java/com/example/amphibians/ui/AmphibiansViewModel.kt
@@ -4,9 +4,13 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
 import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.amphibians.AmphibiansApplication
 import com.example.amphibians.data.Amphibian
-import com.example.amphibians.network.AmphibiansApi
+import com.example.amphibians.data.AmphibiansRepository
 import kotlinx.coroutines.launch
 import retrofit2.HttpException
 import java.io.IOException
@@ -17,20 +21,17 @@ sealed interface AmphibiansUiState {
     object Error : AmphibiansUiState
 }
 
-class AmphibiansViewModel: ViewModel() {
-    var amphibiansUiState : AmphibiansUiState by mutableStateOf(AmphibiansUiState.Loading)
+class AmphibiansViewModel(private val amphibiansRepository: AmphibiansRepository) : ViewModel() {
+    var amphibiansUiState: AmphibiansUiState by mutableStateOf(AmphibiansUiState.Loading)
         private set
 
     fun getAmphibians() {
         viewModelScope.launch {
             amphibiansUiState = try {
-                val retrofitService = AmphibiansApi.retrofitService
-                val amphibians = retrofitService.getAmphibians()
+                val amphibians = amphibiansRepository.getAmphibians()
                 AmphibiansUiState.Success(amphibians)
-
             } catch (e: IOException) {
                 AmphibiansUiState.Error
-
             } catch (e: HttpException) {
                 AmphibiansUiState.Error
             }
@@ -40,4 +41,16 @@ class AmphibiansViewModel: ViewModel() {
     init {
         getAmphibians()
     }
+
+    companion object {
+        val Factory = viewModelFactory {
+            initializer {
+                val application = this[APPLICATION_KEY] as AmphibiansApplication
+                val amphibiansRepository = application.container.amphibiansRepository
+                AmphibiansViewModel(amphibiansRepository)
+            }
+        }
+    }
+
+
 }
